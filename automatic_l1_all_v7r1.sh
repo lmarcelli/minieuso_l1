3e418d1f1a83a2a6a476aa6893e020c776f7edf0
lmarcelli <laura.marcelli@roma2.infn.it>
2020-03-19 11:32:07 +0100

    Add files via upload

---
+++ automatic_l1_all_v7r1.sh
@@ -0,0 +1,40 @@
+#! /bin/sh
+
+echo "RUN 1 - 2019, OCTOBER 7"
+./automatic_l1_v7r1.sh   /media/sf_Mini-EUSO_Analisi/dati_minieuso/iss/20191007/  /media/sf_Mini-EUSO_Analisi/Software_analisi/TLE_zarya/tleISS_20191007_v4.root  59
+
+echo "RUN 2 - 2019, OCTOBER 19"
+./automatic_l1_v7r1.sh   /media/sf_Mini-EUSO_Analisi/dati_minieuso/iss/20191019/  /media/sf_Mini-EUSO_Analisi/Software_analisi/TLE_zarya/tleISS_20191019_20_v4.root  73
+
+echo "RUN 3 - 2019, OCTOBER 25"
+./automatic_l1_v7r1.sh   /media/sf_Mini-EUSO_Analisi/dati_minieuso/iss/20191025/  /media/sf_Mini-EUSO_Analisi/Software_analisi/TLE_zarya/tleISS_20191025_26_v4.root  84   
+
+echo "RUN 4 - 2019, NOVEMBER 6"
+./automatic_l1_v7r1.sh   /media/sf_Mini-EUSO_Analisi/dati_minieuso/iss/20191106/  /media/sf_Mini-EUSO_Analisi/Software_analisi/TLE_zarya/tleISS_20191106_07_v4.root  100
+
+echo "RUN 5 - 2019, NOVEMBER 19"
+./automatic_l1_v7r1.sh   /media/sf_Mini-EUSO_Analisi/dati_minieuso/iss/20191119/  /media/sf_Mini-EUSO_Analisi/Software_analisi/TLE_zarya/tleISS_20191119_20_v4.root  117
+
+echo "RUN 6 - 2019, NOVEMBER 27"
+./automatic_l1_v7r1.sh   /media/sf_Mini-EUSO_Analisi/dati_minieuso/iss/20191127/  /media/sf_Mini-EUSO_Analisi/Software_analisi/TLE_zarya/tleISS_20191127_28_v4.root  128
+
+echo "RUN 7 - 2019, DECEMBER 5"
+./automatic_l1_v7r1.sh   /media/sf_Mini-EUSO_Analisi/dati_minieuso/iss/20191205/  /media/sf_Mini-EUSO_Analisi/Software_analisi/TLE_zarya/tleISS_20191205_06_v4.root  139
+
+echo "RUN 8 - 2019, DECEMBER 30"
+./automatic_l1_v7r1.sh   /media/sf_Mini-EUSO_Analisi/dati_minieuso/iss/20191230/  /media/sf_Mini-EUSO_Analisi/Software_analisi/TLE_zarya/tleISS_20191230_31_v4.root  172
+
+echo "RUN 9 - 2020, JANUARY 8"
+./automatic_l1_v7r1.sh   /media/sf_Mini-EUSO_Analisi/dati_minieuso/iss/20200108/  /media/sf_Mini-EUSO_Analisi/Software_analisi/TLE_zarya/tleISS_20200108_09_v4.root  185
+
+echo "RUN 10 - 2020, JANUARY 14"
+./automatic_l1_v7r1.sh   /media/sf_Mini-EUSO_Analisi/dati_minieuso/iss/20200114/  /media/sf_Mini-EUSO_Analisi/Software_analisi/TLE_zarya/tleISS_20200114_v4.root  193
+
+echo "RUN 11 - 2020, FEBRUARY 21"
+./automatic_l1_v7r1.sh   /media/sf_Mini-EUSO_Analisi/dati_minieuso/iss/20200221/  /media/sf_Mini-EUSO_Analisi/Software_analisi/TLE_zarya/tleISS_20200221_22_v4.root  244
+
+echo "RUN 12 - 2020, MARCH 2"
+./automatic_l1_v7r1.sh   /media/sf_Mini-EUSO_Analisi/dati_minieuso/iss/20200302/  /media/sf_Mini-EUSO_Analisi/Software_analisi/TLE_zarya/tleISS_20200302_03_v4.root  258
+
+
+
